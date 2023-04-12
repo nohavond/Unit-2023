a1fd688fbc832c8625406cdc13e5e7e75c373171
pavlik <sindelp3@mff.cuni.cz>
2023-04-12 11:36:09 +0200

    m

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,11 @@
 import streamlit as st
 from reuest import *
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib3
+from urllib.parse import urlparse, parse_qs
+import html.parser
+hostName = "localhost"
+serverPort = 3000
 
 # Title
 st.title("Faktury")
@@ -23,5 +29,23 @@ def create_interface():
     departments = get_department()
     print(departments)
 
-
 create_interface()
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parse_result = urlparse(self.path)
+        dict_result = parse_qs(parse_result.query)
+        print(dict_result["objectId"])
+        self.send_header('Location','http://localhost:8501')
+        self.end_headers()
+
+webServer = HTTPServer((hostName, serverPort), MyServer)
+print("Server started http://%s:%s" % (hostName, serverPort))
+
+try:
+    webServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+webServer.server_close()
+print("Server stopped.")
