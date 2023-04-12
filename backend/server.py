72dbbb762e67637487a288457f5356fd08088d64
nohavond <nohavond@fit.cvut.cz>
2023-04-12 13:03:35 +0200

    created basic web

--- backend/server.py
+++ backend/server.py
@@ -18,9 +18,11 @@ except:
     st.error("Faktura nebyla nalezena")
     item = []
 
+castka = 0
 for i in item:
     info = get_info(i)
     st.subheader(f"Faktura {info[0]}")
+    castka = float(info[1])
 
     st.text(f"Částka: {info[1]}")
     st.text(info[2])
@@ -29,9 +31,32 @@ for i in item:
 
 @st.cache_data
 def load_departments():
+    dictionary = {}
     departments = get_departments()
+    for department in departments:
+        dictionary[department['nazev']] = department['id']
     options = [departments[0]['nazev'], departments[1]['nazev'], departments[2]['nazev'], departments[3]['nazev']]
-    return options
+    return options, dictionary
 
 
-selected_departments = st.selectbox("Výběr střediska:", load_departments())
+options, departments = load_departments()
+
+selected_options = st.multiselect('Vyberte střediska', options)
+
+
+# creates sliders for every stredisko
+def create_strediska(opt, remaining_price):
+    for i in opt:
+        if (remaining_price > 0):
+            val = st.slider(f"Vyberte částku pro středisko {i}", float(0), max_value=remaining_price, key={i})
+            remaining_price -= val
+    return remaining_price
+
+
+remaining_price = create_strediska(selected_options, castka)
+if remaining_price == 0:
+    if st.button("Udělat vyúčtování"):
+        st.success("Vyúčtování odesláno.")
+else:
+    st.alert("Nerozdělili jste celou částku.")
+
