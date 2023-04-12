0a3e85f4e1eec43fea16b94d4baf31b41b519ca6
nohavond <nohavond@fit.cvut.cz>
2023-04-12 11:27:48 +0200

    craeted basic app

---
+++ backend/reuest.py
@@ -0,0 +1,18 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def get_item(url):
+    item = requests.get(url, auth=HTTPBasicAuth('c6.user4', 'c6.user4.flexi'))
+    item = item.json()['winstrom']['faktura-prijata']
+    return item
+
+
+def get_info(item):
+    return [item['id'], item['sumZklCelkem'], item['firma@showAs'], item['popis']]
+
+
+def get_department():
+    url_department = "https://unit2023.flexibee.eu/c/company6/stredisko.json"
+    departments = requests.get(url_department, auth=HTTPBasicAuth('c6.user4', 'c6.user4.flexi'))
+    return departments.json()
